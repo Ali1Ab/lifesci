01b4e6f1dda6e256bb0b8ff55a5280576d1a76c2
Ali1Ab <aliiabdullahh1@gmail.com>
2025-01-16 11:17:43 +0200

    fixes

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -15,13 +15,6 @@ const Services = () => {
                 <AosComp aos="fade-up">
                     <div className="grid grid-cols-3  gap-4">
 
-                        <div className="col-span-3 lg:col-span-1" >
-                            <div className="bg-[#f6f6f6] rounded-lg p-8 min-h-[320px]">
-                                <Stethoscope className="text-blue-600 w-12 h-12" />
-                                <h4 className=" font-bold my-4 text-2xl">Medical Devices</h4>
-                                <p className="text-lg">We provide cutting-edge regulatory consulting services that ensure the safety, efficacy, and compliance of innovative medical technologies.</p>
-                            </div>
-                        </div>
 
                         <div className="col-span-3 lg:col-span-1" >
                             <div className="bg-[#f6f6f6] rounded-lg p-8 min-h-[320px]">
@@ -33,15 +26,6 @@ const Services = () => {
 
                         <div className="col-span-3 lg:col-span-1" >
                             <div className="bg-[#f6f6f6] rounded-lg p-8 min-h-[320px]">
-                                <Sparkles className="text-pink-500 w-12 h-12" />
-                                <h4 className="font-bold my-4 text-2xl">Cosmetics</h4>
-                                <p className="text-lg">With expertise in regulations and industry standards, we offer customized consulting for cosmetics companies. From product development to market strategies, our guidance ensures compliance and success in this dynamic industry.</p>
-                            </div>
-                        </div>
-
-
-                        <div className="col-span-3 lg:col-span-1" >
-                            <div className="bg-[#f6f6f6] rounded-lg p-8 min-h-[320px]">
                                 <TestTube className="text-green-600 w-12 h-12" />
                                 <h4 className="font-bold my-4 text-2xl">Biotechnology</h4>
                                 <p className="text-lg">We provide strategic regulatory guidance to facilitate the development, approval, and commercialization of biotechnological innovations, thereby advancing healthcare, agriculture, and environmental sustainability.</p>
@@ -49,12 +33,27 @@ const Services = () => {
                         </div>
 
 
+                        <div className="col-span-3 lg:col-span-1" >
+                            <div className="bg-[#f6f6f6] rounded-lg p-8 min-h-[320px]">
+                                <Stethoscope className="text-blue-600 w-12 h-12" />
+                                <h4 className=" font-bold my-4 text-2xl">Medical Devices</h4>
+                                <p className="text-lg">We provide cutting-edge regulatory consulting services that ensure the safety, efficacy, and compliance of innovative medical technologies.</p>
+                            </div>
+                        </div>
 
                         <div className="col-span-3 lg:col-span-1" >
                             <div className="bg-[#f6f6f6] rounded-lg p-8 min-h-[320px]" >
                                 <Atom className="text-red-500 w-12 h-12" />
                                 <h4 className="font-bold my-4 text-2xl">Chemistry</h4>
                                 <p className="text-lg">We provide comprehensive regulatory consulting services to navigate the intricate regulatory landscape, foster responsible chemical stewardship, and promote innovation in the chemical industry.</p>
+                            </div>
+                        </div>
+
+                        <div className="col-span-3 lg:col-span-1" >
+                            <div className="bg-[#f6f6f6] rounded-lg p-8 min-h-[320px]">
+                                <Sparkles className="text-pink-500 w-12 h-12" />
+                                <h4 className="font-bold my-4 text-2xl">Cosmetics</h4>
+                                <p className="text-lg">With expertise in regulations and industry standards, we offer customized consulting for cosmetics companies. From product development to market strategies, our guidance ensures compliance and success in this dynamic industry.</p>
                             </div>
                         </div>
 
